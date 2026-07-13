2b1c550a7b34698fb5cc303c432c5f615bd0dcfb
Amanuel Wondimu <amanuelabb@gmail.com>
2026-07-13 14:13:15 +0300

    feat(rules): detect delegatecall to arbitrary untrusted addresses (iter 2)

--- backend/rules/delegatecall.py
+++ backend/rules/delegatecall.py
@@ -1,3 +1,6 @@
 
 # Progress update on 2026-06-24 - feat(rules): detect delegatecall to arbitrary untrusted addresses
 # Revision: 6
+
+# Progress update on 2026-07-13 - feat(rules): detect delegatecall to arbitrary untrusted addresses (iter 2)
+# Revision: 16
