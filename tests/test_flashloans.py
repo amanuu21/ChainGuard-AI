f372b1448438458f6931b32bd1ece76b9cf91a36
Amanuel Wondimu <amanuelabb@gmail.com>
2026-07-14 20:09:22 +0300

    test(analyzer): verify flash loan attack vector detection accuracy (iter 2)

--- tests/test_flashloans.py
+++ tests/test_flashloans.py
@@ -1,3 +1,6 @@
 
 # Progress update on 2026-07-03 - test(analyzer): verify flash loan attack vector detection accuracy
 # Revision: 9
+
+# Progress update on 2026-07-14 - test(analyzer): verify flash loan attack vector detection accuracy (iter 2)
+# Revision: 19
